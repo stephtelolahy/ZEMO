bfe2b49af6ff3d98f9f70691586b15667b3483d0
Stéphano Telolahy <stephano.telolahy@gmail.com>
2014-11-15 19:42:28 +0000

    first commit

---
+++ andEngine/src/main/obj/local/x86/objs/andengine_shared/src/GLES20Fix.o.d
@@ -0,0 +1,35 @@
+/Users/stephanohuguestelolahy/Documents/Github/GameTutorial/andEngine/src/main/obj/local/x86/objs/andengine_shared/src/GLES20Fix.o: \
+ /Users/stephanohuguestelolahy/Documents/Github/GameTutorial/andEngine/src/main/jni/src/GLES20Fix.c \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/jni.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/sys/cdefs.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/sys/cdefs_elf.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/android/api-level.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/GLES2/gl2.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/GLES2/gl2platform.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/KHR/khrplatform.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/stdint.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/sys/_types.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/machine/_types.h \
+ /Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/sys/_wchar_limits.h
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/jni.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/sys/cdefs.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/sys/cdefs_elf.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/android/api-level.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/GLES2/gl2.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/GLES2/gl2platform.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/KHR/khrplatform.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/stdint.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/sys/_types.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/machine/_types.h:
+
+/Users/stephanohuguestelolahy/Downloads/android-ndk-r10c/platforms/android-19/arch-x86/usr/include/sys/_wchar_limits.h:
---
+++ app/src/main/java/com/telolahy/mariosokoban/MainActivity.java
@@ -0,0 +1,76 @@
+package com.telolahy.mariosokoban;
+
+import android.view.KeyEvent;
+
+import org.andengine.engine.Engine;
+import org.andengine.engine.LimitedFPSEngine;
+import org.andengine.engine.camera.BoundCamera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.WakeLockOptions;
+import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.BaseGameActivity;
+
+import java.io.IOException;
+
+/**
+ * Created by stephanohuguestelolahy on 11/15/14.
+ */
+public class MainActivity extends BaseGameActivity {
+
+    private static final int CAMERA_WIDTH = 320;
+    private static final int CAMERA_HEIGHT = 480;
+
+    private BoundCamera mCamera;
+
+    @Override
+    public Engine onCreateEngine(EngineOptions pEngineOptions) {
+        return new LimitedFPSEngine(pEngineOptions, 60);
+    }
+
+    @Override
+    public EngineOptions onCreateEngineOptions() {
+        mCamera = new BoundCamera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+        EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.PORTRAIT_FIXED, new FillResolutionPolicy(), mCamera);
+        engineOptions.getAudioOptions().setNeedsMusic(true).setNeedsSound(true);
+        engineOptions.getRenderOptions().getConfigChooserOptions().setRequestedMultiSampling(true);
+        engineOptions.setWakeLockOptions(WakeLockOptions.SCREEN_ON);
+        return engineOptions;
+    }
+
+    @Override
+    public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback) throws IOException {
+//        ResourcesManager.prepareManager(mEngine, this, mCamera, getVertexBufferObjectManager());
+        pOnCreateResourcesCallback.onCreateResourcesFinished();
+    }
+
+    @Override
+    public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) throws Exception {
+
+//        SceneManager.getInstance().createSplashScene(pOnCreateSceneCallback);
+    }
+
+    @Override
+    public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
+
+//        mEngine.registerUpdateHandler(new TimerHandler(2f, new ITimerCallback() {
+//            public void onTimePassed(final TimerHandler pTimerHandler) {
+//                mEngine.unregisterUpdateHandler(pTimerHandler);
+//                SceneManager.getInstance().createMenuScene();
+//            }
+//        }));
+        pOnPopulateSceneCallback.onPopulateSceneFinished();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.exit(0);
+    }
+}
