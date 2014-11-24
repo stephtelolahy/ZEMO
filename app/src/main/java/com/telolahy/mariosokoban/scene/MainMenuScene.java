ea6d4f4d77d4d840be828b9f4eeb3ec2e5659d6f
Stéphano Telolahy <stephano.telolahy@gmail.com>
2014-11-24 01:51:02 +0000

    scroll menu

--- app/src/main/java/com/telolahy/mariosokoban/scene/MainMenuScene.java
+++ app/src/main/java/com/telolahy/mariosokoban/scene/MainMenuScene.java
@@ -4,6 +4,8 @@ import com.telolahy.mariosokoban.Constants;
 import com.telolahy.mariosokoban.R;
 import com.telolahy.mariosokoban.manager.SceneManager;
 
+import org.andengine.entity.Entity;
+import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.background.AutoParallaxBackground;
 import org.andengine.entity.scene.background.ParallaxBackground;
 import org.andengine.entity.scene.menu.MenuScene;
@@ -12,23 +14,103 @@ import org.andengine.entity.scene.menu.item.TextMenuItem;
 import org.andengine.entity.scene.menu.item.decorator.ScaleMenuItemDecorator;
 import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.entity.sprite.Sprite;
+import org.andengine.entity.text.Text;
+import org.andengine.input.touch.TouchEvent;
+import org.andengine.input.touch.detector.ScrollDetector;
+import org.andengine.input.touch.detector.SurfaceScrollDetector;
+import org.andengine.util.adt.color.Color;
 
 /**
  * Created by stephanohuguestelolahy on 11/16/14.
  */
-public class MainMenuScene extends BaseScene {
+public class MainMenuScene extends BaseScene implements ScrollDetector.IScrollDetectorListener {
 
-    private MenuScene mMenuChildScene;
-    private AnimatedSprite mBackgroundPlayer;
+    // ===========================================================
+    // Constants
+    // ===========================================================
+
+    private static final int LEVEL_COLUMNS_PER_SCREEN = 4;
+    private static final int LEVEL_ROWS_PER_SCREEN = 3;
+    private static final int LEVEL_PADDING = 50;
+    private static final int LEVEL_BOX_SIZE = 50;
+
+    private static final int LEVELS_COUNT = 30;
 
     private static final int MENU_PLAY = 0;
-    private static final int MENU_HELP = 1;
+    private static final int MENU_OPTIONS = 1;
+
+    // ===========================================================
+    // Fields
+    // ===========================================================
+
+    // main menu scene
+    private MenuScene mMenuChildScene;
+
+    // Scrolling
+    private SurfaceScrollDetector mScrollDetector;
+
+    private float mMinX;
+    private float mMaxX;
+    private float mOffsetX;
+
+    //This value will be loaded from whatever method used to store data.
+    private int mMaxLevelReached = 7;
+
+    private Entity mLevelSelectionLayer;
+
+    // ===========================================================
+    // Constructors
+    // ===========================================================
+
+    // ===========================================================
+    // Getter & Setter
+    // ===========================================================
+
+    // ===========================================================
+    // Methods for/from SuperClass/Interfaces
+    // ===========================================================
+
 
     @Override
     public void createScene() {
 
         createBackground();
         createMenuChildScene();
+        createLevelSelection();
+        setupTouchGesture();
+        startMusic();
+    }
+
+    @Override
+    public void onBackKeyPressed() {
+
+        System.exit(0);
+    }
+
+    @Override
+    public void disposeScene() {
+
+        if (mResourcesManager.menuMusic.isPlaying()) {
+            mResourcesManager.menuMusic.pause();
+        }
+
+        this.detachSelf();
+        this.dispose();
+    }
+
+    // ===========================================================
+    // Methods
+    // ===========================================================
+
+    private void setupTouchGesture() {
+
+        this.setOnSceneTouchListener(new SurfaceScrollDetector(this));
+        this.setTouchAreaBindingOnActionDownEnabled(true);
+        this.setTouchAreaBindingOnActionMoveEnabled(true);
+        this.setOnSceneTouchListenerBindingOnActionDownEnabled(true);
+    }
+
+    private void startMusic() {
 
         if (!mResourcesManager.menuMusic.isPlaying()) {
             mResourcesManager.menuMusic.setLooping(true);
@@ -46,9 +128,9 @@ public class MainMenuScene extends BaseScene {
         setBackground(autoParallaxBackground);
 
         final int groundY = 28;
-        mBackgroundPlayer = new AnimatedSprite(Constants.SCREEN_WIDTH / 2, mResourcesManager.menuPlayerTextureRegion.getHeight() / 2 + groundY, mResourcesManager.menuPlayerTextureRegion, mVertexBufferObjectManager);
-        mBackgroundPlayer.animate(new long[]{250, 250, 250, 250}, 0, 3, true);
-        attachChild(mBackgroundPlayer);
+        final AnimatedSprite player = new AnimatedSprite(Constants.SCREEN_WIDTH / 2, mResourcesManager.menuPlayerTextureRegion.getHeight() / 2 + groundY, mResourcesManager.menuPlayerTextureRegion, mVertexBufferObjectManager);
+        player.animate(new long[]{250, 250, 250, 250}, 0, 3, true);
+        attachChild(player);
     }
 
     private void createMenuChildScene() {
@@ -57,8 +139,8 @@ public class MainMenuScene extends BaseScene {
 
         TextMenuItem playTextMenuItem = new TextMenuItem(MENU_PLAY, mResourcesManager.font, mActivity.getResources().getString(R.string.play), mVertexBufferObjectManager);
         IMenuItem playMenuItem = new ScaleMenuItemDecorator(playTextMenuItem, 1.2f, 1);
-        TextMenuItem helpTextMenuItem = new TextMenuItem(MENU_HELP, mResourcesManager.font, mActivity.getResources().getString(R.string.options), mVertexBufferObjectManager);
-        IMenuItem helpMenuItem = new ScaleMenuItemDecorator(helpTextMenuItem, 1.2f, 1);
+        TextMenuItem optionsTextMenuItem = new TextMenuItem(MENU_OPTIONS, mResourcesManager.font, mActivity.getResources().getString(R.string.options), mVertexBufferObjectManager);
+        IMenuItem helpMenuItem = new ScaleMenuItemDecorator(optionsTextMenuItem, 1.2f, 1);
         mMenuChildScene.addMenuItem(playMenuItem);
         mMenuChildScene.addMenuItem(helpMenuItem);
 
@@ -71,9 +153,10 @@ public class MainMenuScene extends BaseScene {
 
                 switch (pMenuItem.getID()) {
                     case MENU_PLAY:
-                        SceneManager.getInstance().createGameScene();
+                        mLevelSelectionLayer.setVisible(true);
+                        mMenuChildScene.setVisible(false);
                         return true;
-                    case MENU_HELP:
+                    case MENU_OPTIONS:
                         return true;
                     default:
                         return false;
@@ -84,23 +167,116 @@ public class MainMenuScene extends BaseScene {
         setChildScene(mMenuChildScene);
     }
 
+    private void createLevelSelection() {
+
+        mLevelSelectionLayer = new Entity(0, 0);
+        mLevelSelectionLayer.setVisible(false);
+        attachChild(mLevelSelectionLayer);
+
+        // calculate the amount of required columns for the level count
+        int levelsPerPage = (LEVEL_ROWS_PER_SCREEN * LEVEL_COLUMNS_PER_SCREEN);
+        int totalPages = (LEVELS_COUNT / levelsPerPage) + (LEVELS_COUNT % levelsPerPage == 0 ? 0 : 1);
+
+        // Calculate space between each level square
+        int spaceBetweenRows = (Constants.SCREEN_HEIGHT / LEVEL_ROWS_PER_SCREEN) - LEVEL_PADDING;
+        int spaceBetweenColumns = (Constants.SCREEN_WIDTH / LEVEL_COLUMNS_PER_SCREEN) - LEVEL_PADDING;
+
+        //Current Level Counter
+        int iLevel = 1;
+
+        // Create the level selectors, one page at a time
+        for (int page = 0; page < totalPages; page++) {
+
+            int pageX = page * Constants.SCREEN_WIDTH;
+
+            //Create the Level selectors, one row at a time.
+            for (int y = 0; y < LEVEL_ROWS_PER_SCREEN && iLevel <= LEVELS_COUNT; y++) {
+
+                int boxY = LEVEL_PADDING + spaceBetweenRows * y;
+
+                for (int x = 0; x < LEVEL_COLUMNS_PER_SCREEN && iLevel <= LEVELS_COUNT; x++) {
+
+                    //On Touch, save the clicked level in case it's a click and not a scroll.
+                    final int levelToLoad = iLevel;
+                    int boxX = pageX + LEVEL_PADDING + spaceBetweenColumns * x;
+
+                    // Create the rectangle. If the level selected
+                    // has not been unlocked yet, don't allow loading.
+                    Rectangle box = new Rectangle(boxX, boxY, LEVEL_BOX_SIZE, LEVEL_BOX_SIZE, mVertexBufferObjectManager) {
+                        @Override
+                        public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
+
+                            if (pSceneTouchEvent.getAction() == TouchEvent.ACTION_DOWN) {
+
+                                if (levelToLoad < mMaxLevelReached) {
+                                    loadLevel(levelToLoad);
+                                }
+                                return true;
+                            }
+
+                            return false;
+                        }
+                    };
+
+                    Color color = iLevel >= mMaxLevelReached ? new Color(0f, 0f, 1f, 0.5f) : new Color(0f, 1f, 0f, 0.5f);
+                    box.setColor(color);
+
+                    mLevelSelectionLayer.attachChild(box);
+
+                    //Center for different font size
+                    int textMargin = iLevel < 10 ? 18 : 10;
+                    mLevelSelectionLayer.attachChild(new Text(boxX + textMargin, boxY + 15, mResourcesManager.font, String.valueOf(iLevel), mVertexBufferObjectManager));
+
+                    this.registerTouchArea(box);
+
+                    iLevel++;
+                }
+            }
+        }
+
+        //Set the max scroll possible, so it does not go over the boundaries.
+        mMinX = 0;
+        mMaxX = totalPages * Constants.SCREEN_WIDTH;
+//        mMaxY = boxY - CAMERA_HEIGHT + 200;
+    }
+
+    //Here is where you call the level load.
+    private void loadLevel(final int iLevel) {
+        if (iLevel != -1) {
+
+            SceneManager.getInstance().createGameScene();
+        }
+    }
+
+
+    // ===========================================================
+    // Implemented interfaces
+    // ===========================================================
+
 
     @Override
-    public void onBackKeyPressed() {
+    public void onScroll(ScrollDetector pScollDetector, int pPointerID, float pDistanceX, float pDistanceY) {
 
-        System.exit(0);
+        if (!mLevelSelectionLayer.isVisible()) {
+            return;
+        }
+
+        float newX = mOffsetX - pDistanceX;
+        if (newX < mMinX || newX > mMaxX) {
+            return;
+        }
+
+        mLevelSelectionLayer.setPosition(-newX, 0);
+        mOffsetX = newX;
     }
 
     @Override
-    public void disposeScene() {
+    public void onScrollStarted(ScrollDetector pScollDetector, int pPointerID, float pDistanceX, float pDistanceY) {
 
-        if (mResourcesManager.menuMusic.isPlaying()) {
-            mResourcesManager.menuMusic.pause();
-        }
+    }
 
-        mBackgroundPlayer.detachSelf();
+    @Override
+    public void onScrollFinished(ScrollDetector pScollDetector, int pPointerID, float pDistanceX, float pDistanceY) {
 
-        this.detachSelf();
-        this.dispose();
     }
 }
