8c8e12fd8937fe58e35136db9ad9b248009b9d10
Stéphano Telolahy <stephano.telolahy@gmail.com>
2014-12-04 09:52:00 +0000

    align credits

--- app/src/main/java/com/telolahy/mariosokoban/scene/CreditsScene.java
+++ app/src/main/java/com/telolahy/mariosokoban/scene/CreditsScene.java
@@ -41,25 +41,23 @@ public class CreditsScene extends BaseScene {
     @Override
     protected void onCreateScene(int... params) {
 
-        mBackground = new Sprite(Constants.SCREEN_WIDTH / 2, Constants.SCREEN_HEIGHT / 2, mResourcesManager.menuCreditsBackgroundTextureRegion, mVertexBufferObjectManager);
+        mBackground = new Sprite(Constants.SCREEN_WIDTH / 2, mResourcesManager.menuCreditsBackgroundTextureRegion.getHeight() / 2, mResourcesManager.menuCreditsBackgroundTextureRegion, mVertexBufferObjectManager);
         attachChild(mBackground);
 
-        final int topY = Constants.SCREEN_HEIGHT / 2;
+        attachChild(new Text(Constants.SCREEN_WIDTH / 2, 400, mResourcesManager.menuCreditsFont, mActivity.getResources().getString(R.string.game_developer), mVertexBufferObjectManager));
+        attachChild(new Text(Constants.SCREEN_WIDTH / 2, 360, mResourcesManager.menuLevelFont, mActivity.getResources().getString(R.string.game_developer_value), mVertexBufferObjectManager));
 
-        attachChild(new Text(Constants.SCREEN_WIDTH / 2, topY, mResourcesManager.menuCreditsFont, mActivity.getResources().getString(R.string.game_developer), mVertexBufferObjectManager));
-        attachChild(new Text(Constants.SCREEN_WIDTH / 2, topY - 40, mResourcesManager.menuLevelFont, mActivity.getResources().getString(R.string.game_developer_value), mVertexBufferObjectManager));
+        attachChild(new Text(Constants.SCREEN_WIDTH / 2, 300, mResourcesManager.menuCreditsFont, mActivity.getResources().getString(R.string.game_designer), mVertexBufferObjectManager));
+        attachChild(new Text(Constants.SCREEN_WIDTH / 2, 260, mResourcesManager.menuLevelFont, mActivity.getResources().getString(R.string.game_designer_value), mVertexBufferObjectManager));
 
-        attachChild(new Text(Constants.SCREEN_WIDTH / 2, topY - 100, mResourcesManager.menuCreditsFont, mActivity.getResources().getString(R.string.game_designer), mVertexBufferObjectManager));
-        attachChild(new Text(Constants.SCREEN_WIDTH / 2, topY - 140, mResourcesManager.menuLevelFont, mActivity.getResources().getString(R.string.game_designer_value), mVertexBufferObjectManager));
-
-        attachChild(new Text(Constants.SCREEN_WIDTH / 2, topY - 200, mResourcesManager.menuCreditsFont, mActivity.getResources().getString(R.string.game_engine), mVertexBufferObjectManager));
-        attachChild(new Text(Constants.SCREEN_WIDTH / 2, topY - 240, mResourcesManager.menuLevelFont, mActivity.getResources().getString(R.string.game_engine_value), mVertexBufferObjectManager));
+        attachChild(new Text(Constants.SCREEN_WIDTH / 2, 200, mResourcesManager.menuCreditsFont, mActivity.getResources().getString(R.string.game_engine), mVertexBufferObjectManager));
+        attachChild(new Text(Constants.SCREEN_WIDTH / 2, 160, mResourcesManager.menuLevelFont, mActivity.getResources().getString(R.string.game_engine_value), mVertexBufferObjectManager));
 
         try {
             PackageInfo pInfo = mActivity.getPackageManager().getPackageInfo(mActivity.getPackageName(), 0);
             String versionName = pInfo.versionName;
             String versionDescription = mActivity.getResources().getString(R.string.app_name) + " version " + versionName;
-            attachChild(new Text(Constants.SCREEN_WIDTH / 2, topY - 300, mResourcesManager.menuCreditsFont, versionDescription, mVertexBufferObjectManager));
+            attachChild(new Text(Constants.SCREEN_WIDTH / 2, 100, mResourcesManager.menuCreditsFont, versionDescription, mVertexBufferObjectManager));
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
@@ -67,7 +65,7 @@ public class CreditsScene extends BaseScene {
         Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
         String gameStudioDescription = mActivity.getResources().getString(R.string.game_studio) + year;
-        attachChild(new Text(Constants.SCREEN_WIDTH / 2, topY - 340, mResourcesManager.menuCreditsFont, gameStudioDescription, mVertexBufferObjectManager));
+        attachChild(new Text(Constants.SCREEN_WIDTH / 2, 60, mResourcesManager.menuCreditsFont, gameStudioDescription, mVertexBufferObjectManager));
     }
 
     @Override
